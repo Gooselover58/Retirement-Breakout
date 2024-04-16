9c31f4d71da3d1a1837bf45727d4733524c91afe
Gooselover58 <alexander.ejeblad@elev.ga.lbs.se>
2024-04-16 14:31:36 +0200

    wadawd

--- Assets/Scripts/WeaponPivot.cs
+++ Assets/Scripts/WeaponPivot.cs
@@ -7,12 +7,14 @@ public class WeaponPivot : MonoBehaviour
     private Transform player;
     private Rigidbody2D rb;
     private Camera cam;
+    private Vector2 ogOffset;
 
     void Start()
     {
         player = WeaponManager.Instance.player.transform;
         rb = GetComponent<Rigidbody2D>();
         cam = Camera.main;
+        ogOffset = transform.position - player.position;
     }
 
     void FixedUpdate()
@@ -21,5 +23,6 @@ public class WeaponPivot : MonoBehaviour
         Vector2 dir = rb.position - mouse;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
+        rb.position = (Vector2)player.transform.position + ogOffset;
     }
 }
