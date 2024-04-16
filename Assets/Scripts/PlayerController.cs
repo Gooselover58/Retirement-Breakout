17fe9758e9e64096ee6b8cbe3707f680106d4346
Snofelius <alfons.nihlen@elev.ga.lbs.se>
2024-04-16 14:15:21 +0200

    Baby one more time - Tenatious D

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
+    [SerializeField] float gottaGoFast;
 
     void Start()
     {
-        GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector2 moveIt = new Vector2(x, y) * gottaGoFast;
+
+        rb.velocity = moveIt;
     }
 }
